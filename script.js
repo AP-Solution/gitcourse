142f20d00915204432a8aae3d3003deb13155b2f
AP_Solution <haunt.beta@gmail.com>
2021-01-18 22:14:59 +0200

    download css and small fixes

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
 let header = document.querySelector('header');
-let cont = document.createElement('div');
-cont.cssText = `
-	width: 50%;
-	height: 200px;
-	background-color: red;
-`;
-
-header.style.cssText = `
-	background-color: lightblue;
-	width: 100%;
-	min-height: 50vh;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-`;
-
-header.append(cont);
