ea123951c6ba6cd939a120387650feb4452942e6
AP_Solution <haunt.beta@gmail.com>
2021-01-19 22:38:11 +0200

    create slider

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-let header = document.querySelector('header');
+const imgBx = document.querySelector('.imgBx');
+		const slides = imgBx.querySelectorAll('img');
+		const nextSlide = document.querySelector('.next');
+		const prevSlide = document.querySelector('.prev');
+		let i = 0;
+
+		console.log(slides[0]);
+
+function autoSlide () {
+	slides[i].classList.remove('active');
+	i = (i + 1) % slides.length;
+	slides[i].classList.add('active');
+}
+
+		nextSlide.addEventListener('click', autoSlide);
+
+		prevSlide.addEventListener('click', () => {
+			slides[i].classList.remove('active');
+			i = (i - 1 + slides.length) % slides.length;
+			slides[i].classList.add('active');
+		});
+
+setInterval(autoSlide, 5000);
